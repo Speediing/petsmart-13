03d1e9554df0e406950a7fce12ca5b6b1ac667a3
Jason Wiker <Jason.wiker@vercel.com>
2023-01-30 12:22:44 -0700

    InfoHeader

--- apps/web/app/InfoHeader.tsx
+++ apps/web/app/InfoHeader.tsx
@@ -11,11 +11,11 @@ async function InfoHeader() {
   const shouldShowHeader = await get("shouldShowBanner");
   if (!shouldShowHeader) return <div></div>;
   return (
-    <div className="h-24 bg-[#102641] text-white py-8 px-11 text-sm flex flex-col justify-center">
-      <p className="font-semibold">
-        Your complete financial picture on a single platform
+    <div className="flex flex-col justify-center h-[48px] py-8 text-xl text-[#c8102e] bg-white px-11">
+      <p className="font-semibold text-center">
+        IN-STORE & CURBSIDE PICKUP: shop online & pick up in store for free!
+        Shop now
       </p>
-      <p>Connect with more than 20k financial institutions</p>
     </div>
   );
 }
